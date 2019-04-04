4f1c2340be72f13af530cbc5a2cf157cd169f703
Leon Chou <leon.chou@nyu.edu>
2019-04-04 19:49:05 -0400

    added crypto and other todos to server.py

--- server/app.py
+++ server/app.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from uuid import uuid4
+
+from flask import Flask, jsonify
+from Crypto.Cipher import AES
+from Crypto import Random
 
 app = Flask(__name__)
+db = {} # we're using a dictionary in lieu of an actual database for simplicity's sake
+
+# TODO: intiialize random
 
 @app.route("/", methods=['GET'])
 def index():
@@ -12,17 +19,89 @@ def index():
 Register will receive the client's public key
 
 '''
+@app.route('/register', methods=['POST'])
+def register():
+
+  # TODO: get pubkey from form-body or json post data
+
+  # TODO: generate token here
+  token = None
+
+  return jsonify({
+    success: True,
+    token: token,
+  })
+
+# TODO: gate methods below with an identity route that checks identities
+'''
+check_request:
+
+check "Authorization" header to grab the Bearer token
+(we're basically implementing OAuth1.0 for this server)
+'''
+
 
 # TODO: POST /token
 '''
 token will return tokens to clients
 token format:
+
+{
+  token: <string>
+}
+
+the token value is generated using:
+generate_token(payload, pubkey)
 {
   uid: <string>,
   iat: <datetime>,
 }
-
 '''
+@app.route('/token', methods=['POST'])
+def token():
+  uid = uuid4()
+
+  payload = {
+    
+  }
+
+  token = None
+
+  return jsonify({
+    success: True,
+    token: token,
+  })
+
+# TODO: POST /upload
+'''
+POST /upload
+checks identity
+generates a fileid
+'''
+@app.route('/upload', methods=['POST'])
+def upload():
+  fileid = uuid4()
+
+  # TODO: generate random filename
+
+  return jsonify({
+    success: True,
+    fileid:
+  })
+
+# TODO: GET /download
+'''
+GET /download
+request using the token provided above
+'''
+@app.route('/download/<string:id>', methods=['GET'])
+def download(id):
+  
+  # TODO: get fname by id
+  fname = None
+
+  return send_file(fname)
+
 
 
 if __name__ == '__main__':
